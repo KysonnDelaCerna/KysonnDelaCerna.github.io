feed3c4c9c7a363504d7171a25366df0e7efce21
KysonnDelaCerna <kysonndelacerna@gmail.com>
2019-07-27 15:25:36 +0800

    added navigation, removed navbar

---
+++ scripts/navigation.js
@@ -0,0 +1,21 @@
+let keys = [];
+
+document.addEventListener("keyup",keyup,false);
+
+function keyup(e) {
+    if (!e) e= event;
+    if (keys.length >= 50)
+        keys = [];
+    if (e.key !== "Enter")
+        keys.push(e.key);
+    else
+        checkKeys();
+}
+
+function checkKeys () {
+    if (keys.toString() === "g,i,t,h,u,b") {
+        window.location.href = "http://www.github.com/KysonnDelaCerna";
+    }
+
+    keys = [];
+}
