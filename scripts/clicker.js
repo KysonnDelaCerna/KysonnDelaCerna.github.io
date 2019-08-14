ed8d304a6982e46bc7b8292c50b06eded7eb1bd0
KysonnDelaCerna <kysonndelacerna@gmail.com>
2019-08-14 18:20:24 +0800

    fuck trees

--- scripts/clicker.js
+++ scripts/clicker.js
@@ -1,37 +1,28 @@
-let wood = 0;
-let woodPerClick = 1;
-let trees = 1000000;
+let player = {
+    wood: "0",
+    woodPerClick: "1",
+    house: "false"
+}
 
 function chopWood () {
-    if (trees <= 0)
-        return;
-    
     wood += woodPerClick;
-    trees -= 1;
 
     update();
 }
 
 function saveData () {
-    localStorage.setItem('wood', wood);
-    localStorage.setItem('woodPerClick', woodPerClick);
-    localStorage.setItem('trees', trees);
+    localStorage.setItem('player', JSON.stringify(player));
 }
 
 function loadData () {
-    if (typeof(localStorage.wood) !== 'undefined')
-        wood = parseInt(localStorage.wood);
-    if (typeof(localStorage.woodPerClick) !== 'undefined')
-        woodPerClick = parseInt(localStorage.woodPerClick);
-    if (typeof(localStorage.trees) !== 'undefined')
-        trees = parseInt(localStorage.trees);
+    if (typeof(localStorage.player) !== 'undefined')
+        player = JSON.parse(localStorage.player)
 
     update();
 }
 
 function update () {
-    document.getElementById('wood').innerHTML = 'Wood: ' + wood;
-    document.getElementById('trees').innerHTML = 'Trees: ' + trees;
+    document.getElementById('wood').innerHTML = 'Wood: ' + player.wood;
 }
 
 loadData();
