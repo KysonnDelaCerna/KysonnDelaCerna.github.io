8c2800047c8b77288cf9a5272cecc965d9130f56
KysonnDelaCerna <Kysonndelacerna@gmail.com>
2019-09-26 13:58:00 +0800

    GIBBBEEEEEHHHHH

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -24,6 +24,25 @@ function checkKeys () {
         if (document.title !== 'Kysonn Dela Cerna')
             window.location.href = '/';
     }
+    else if (keys.toString() === 'g,i,b,b,y') {
+        if (document.title == 'Kysonn Dela Cerna') {
+            let elem = document.getElementById("gibby");
+            let audio = new Audio('assets/gibby.mp3');
+            audio.play(); 
+            let pos = -50;
+            let id = setInterval(frame, 4);
+            function frame() {
+                if (pos == 150) {
+                    clearInterval(id);
+                    elem.style.marginTop = '-50%';
+                } else {
+                    pos++;
+                    elem.style.marginTop = pos + '%';
+                }
+            }
+            console.log("GIBBY");
+        }
+    }
 
     keys = [];
 }
