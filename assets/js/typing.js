3e31744f8675250ae1809754b51c2dd3b994f729
Kysonn Dela Cerna <kysonndelacerna@gmail.com>
2022-09-20 16:33:03 +0800

    fixed typing

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,19 +1,19 @@
 const title = document.getElementById('title');
 const subtitle = document.getElementById('subtitle');
 
-const titleText = `<Kysonn\xa0Dela\xa0Cerna/>\xa0`;
-const subtitleText = `Computer\xa0Scientist\xa0`;
+const titleText = `<Kysonn Dela Cerna/>`;
+const subtitleText = `Computer Scientist`;
 
 async function type () {
     for (let i = 0; i < titleText.length; i++) {
-        title.innerText += titleText[i];
+        title.innerText = titleText.slice(0, i + 1);
         await new Promise(r => setTimeout(r, 50 + Math.floor(Math.random() * 50)));
     }
 
     await new Promise(r => setTimeout(r, 500 + Math.floor(Math.random() * 200)));
 
     for (let i = 0; i < subtitleText.length; i++) {
-        subtitle.innerText += subtitleText[i];
+        subtitle.innerText = subtitleText.slice(0, i + 1);
         await new Promise(r => setTimeout(r, 50 + Math.floor(Math.random() * 50)));
     }
 }
